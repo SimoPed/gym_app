9db69e89100c64011a690e9696fa194167c520fc
SimoPed <103127889+SimoPed@users.noreply.github.com>
2023-01-06 12:30:13 +0100

    aggiustare collegamento firebase

---
+++ lib/details_exercises.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsExercises extends StatefulWidget {
+  const DetailsExercises({Key? key}) : super(key: key);
+
+  @override
+  State<DetailsExercises> createState() => _DetailsExercisesState();
+}
+
+class _DetailsExercisesState extends State<DetailsExercises> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
